08348fb7ed6f9b7c92d5f22d2228599125ceb9f7
Ribamar FS <ribafs.com>
2019-05-28 13:28:05 +0000

    Primeiro commit

---
+++ scripts/cake.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# Criado por Ribamar FS - http://ribafs.org
+# Sintaxe: cd /var/www/html; cake.sh nomeapp
+composer create-project --prefer-dist cakephp/app $1
+
---
+++ scripts/perms.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Criado por Ribamar FS - https://ribafs.org
+# Sintaxe: 
+# sudo perms.sh nomedosubdir (varrerá somente o subdir do /var/www/html)
+# sudo perms (varrerá todo o /var/www/html)
+clear;
+echo "Aguarde enquanto configuro as permissões do /var/www/html/$1";
+echo "";
+chown -R vagrant:www-data /var/www/html/$1;
+find /var/www/html/$1 -type d -exec chmod 775 {} \;
+find /var/www/html/$1 -type f -exec chmod 664 {} \;
+echo "";
+# Manter bin/cake executável
+file=/var/www/html/$1/bin/cake
+if [ -f $file ];
+then
+    chmod +x $file
+else
+    echo Arquivo $file não existe
+fi
+echo "Concluído!";
+
